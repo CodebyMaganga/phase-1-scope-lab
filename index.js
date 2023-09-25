3cc07b8caf97b003a6cccb4331068906fb600e8a
Maganga Mwambonu <codebymaganga@gmail.com>
2023-09-25 13:14:01 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName ="Bazu"
+const leastFavoriteCustomer = 'My ex'
+
+function changeToUppercase(customerName){
+    return customerName.toUpperCase()
+}
+console.log(changeToUppercase(customerName))
+
+function setBestCustomer(){
+    var bestCustomer = 'not bob'
+    return bestCustomer
+}
+console.log(setBestCustomer())
+
+function overwriteBestCustomer(){
+    bestCustomer = 'not me'
+    return bestCustomer
+}
+
+console.log(overwriteBestCustomer())
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'U'
+    return leastFavoriteCustomer
+}
+
+console.log(changeLeastFavoriteCustomer())
